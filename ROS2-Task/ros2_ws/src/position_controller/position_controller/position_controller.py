89a3c5624abd0c95ca413b803f4260001596ba37
DannyyyL <lichtindan@gmail.com>
2026-09-14 22:33:16 -0400

    Add emergency stop subscription

--- ROS2-Task/ros2_ws/src/position_controller/position_controller/position_controller.py
+++ ROS2-Task/ros2_ws/src/position_controller/position_controller/position_controller.py
@@ -1,24 +1,40 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 class PositionController(Node):
     def __init__(self):
         super().__init__("position_controller")
-        self.subscription = self.create_subscription(
+
+        #Movement command
+        self.movement_subscription = self.create_subscription(
             Twist,
             "movement_command",
             self.movement_callback,
             10
         )
-        self.subscription
         self.x = 0
         self.y = 0
 
+        #Emergency stop
+        self.emergency_subscription = self.create_subscription(
+            Bool,
+            "emergency_stop",
+            self.emergency_callback,
+            10
+        )
+        self.e_brake = False
+
     def movement_callback(self, msg):
-        self.x += msg.linear.x
-        self.y += msg.linear.y
+        if not self.e_brake:
+            self.x += msg.linear.x
+            self.y += msg.linear.y
         self.get_logger().info(f"x: {self.x}, y: {self.y}")
+
+    def emergency_callback(self, msg):
+        self.e_brake = msg.data
+        self.get_logger().info(f"Emergency brake: {self.e_brake}")
     
 
 def main(args=None):
